a59777a6a0915bd5f9d680b8e5838af9fa58201d
nbs <nbs@DESKTOP-LTG5TAS.mshome.net>
2020-10-14 16:16:51 -0500

    I create a gitIgnore and made some code

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -3,9 +3,13 @@ public class MySweetProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+        System.out.println(" Eclipse shut up");
+        int i = 0; //Initialization
+        do {
+            System.out.println(" take those " + i + " punches Bascuñan");
+            i++;
+        } while (i < 100); //Useful when u need to do it at least once
+        }
+	
 }
-
 
